28fa17d63e6e7fff248e2d3b905dc290d212903c
Mohamed <mohamedelbeshbeshy98@gmail.com>
2025-02-02 20:15:26 +0200

    day6

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,3 @@
-
 import { CartCounterService } from './../services/cart-counter.service';
 import { Component } from '@angular/core';
 import { Product } from '../types/product';
@@ -6,11 +5,12 @@ import { Product } from '../types/product';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css',
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
   cartItems: Product[] = [];
-  counter : number = 0;
+  counter: number = 0;
+  totalPrice: number = 0; 
 
   constructor(
     private CartCounterService: CartCounterService,
@@ -18,31 +18,39 @@ export class CartComponent {
 
   ngOnInit() {
     this.loadCart();
-
   }
-  loadCart(){
+
+  loadCart() {
     this.cartItems = this.CartCounterService.getCartItems();
     this.counter = this.cartItems.length;
-    for(let item of this.cartItems){
+
+    for (let item of this.cartItems) {
       item.counter = 1;
     }
   }
-  removeItem(item : Product){
+
+  removeItem(item: Product) {
     this.CartCounterService.removeItemFromCart(item);
     this.loadCart();
-
   }
 
   increaseCounter(item: Product) {
     if (item.counter < item.stock) {
       item.counter++;
+      this.calculateTotalPrice(); 
     }
   }
-  
+
   decreaseCounter(item: Product) {
     if (item.counter && item.counter > 1) {
       item.counter--;
+      this.calculateTotalPrice(); 
     }
   }
-  
+
+  calculateTotalPrice() {
+    this.totalPrice = this.cartItems.reduce((total, item) => {
+      return total + item.price * item.counter;
+    }, 0);
+  }
 }
